c14e3b54eb1c5808407309d506557b1100008009
ze-zeh <chojh922@gmail.com>
2024-02-18 21:17:47 +0900

    프로젝트 생성

---
+++ app/src/main/java/jjh/movie/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package jjh.movie.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
