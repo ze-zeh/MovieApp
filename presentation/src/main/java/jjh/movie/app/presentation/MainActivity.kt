ef5549ee7ab0c3a92d26b377d8642294e642c0bb
ze-zeh <chojh922@gmail.com>
2024-02-22 17:19:17 +0900

    #2 바텀 네비게이션 추가

--- presentation/src/main/java/jjh/movie/app/presentation/MainActivity.kt
+++ presentation/src/main/java/jjh/movie/app/presentation/MainActivity.kt
@@ -1,11 +1,26 @@
 package jjh.movie.app.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import jjh.movie.app.presentation.base.BaseViewBindingActivity
+import jjh.movie.app.presentation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseViewBindingActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    private lateinit var host: NavHostFragment
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        host = supportFragmentManager.findFragmentById(R.id.container_main) as NavHostFragment
+        navController = host.navController
+
+        binding.bottomNavBar.setupWithNavController(navController)
     }
 }
