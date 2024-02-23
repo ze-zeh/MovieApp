3b7d8353557b778e04fc480b572dbc3bad361659
ze-zeh <chojh922@gmail.com>
2024-02-23 15:22:35 +0900

    #2 정우 코드 리뷰 반영

--- presentation/src/main/java/jjh/movie/app/presentation/MainActivity.kt
+++ presentation/src/main/java/jjh/movie/app/presentation/MainActivity.kt
@@ -1,24 +1,29 @@
 package jjh.movie.app.presentation
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import jjh.movie.app.presentation.base.BaseViewBindingActivity
 import jjh.movie.app.presentation.databinding.ActivityMainBinding
 
-class MainActivity : BaseViewBindingActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         initNavigation()
     }
 
     private fun initNavigation() {
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.container_main) as NavHostFragment
+        navHostFragment =
+            supportFragmentManager.findFragmentById(binding.containerMain.id) as NavHostFragment?
+                ?: return
         navController = navHostFragment.navController
 
         binding.bottomNavBar.setupWithNavController(navController)
